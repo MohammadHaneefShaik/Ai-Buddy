2b49f4891ea3dd049778616b862613eac716a7a2
MOHAMMAD HANEEF S <shaikmohammadhaneef01@gmail.com>
2026-05-27 13:47:14 +0530

    Update api.tsx

--- frontend/src/helpers/api.tsx
+++ frontend/src/helpers/api.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3000/api/v1";
+axios.defaults.baseURL = "/_/backend/api/v1";
 axios.defaults.withCredentials = true;
 
 export const loginUser = async (email: string, password: string) => {
