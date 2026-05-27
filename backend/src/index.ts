7055fba29275f6d07e397ca5a2ba80a833744d77
MOHAMMAD HANEEF S <shaikmohammadhaneef01@gmail.com>
2026-05-27 13:32:14 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,15 @@
 import app from "./app.js";
 import { connectToDatabase } from "./db/connection.js";
 
-//connections and listners
+// Connections and listeners
 
 const PORT = process.env.PORT || 3000;
+
 connectToDatabase()
   .then(() => {
-    app.listen(PORT, () => console.log("Server started & Connected To Database 🤟"),
-      console.log(PORT));
+    app.listen(PORT, () => {
+      console.log(`Server started & Connected To Database 🤟`);
+      console.log(`Running on port ${PORT}`);
+    });
   })
   .catch((err) => console.log(err));
